a66e0f44f5ed41972f207eec314f535591e7d8e4
Dullah Mtavya <mtavya420@gmail.com>
2023-05-18 22:21:18 +0900

    remove secretkey

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,7 +4,6 @@ export default axios.create({
   baseURL: "https://api.jamendo.com/v3.0",
   params: {
     client_id: "8f6bfd02",
-    client_secret: "2445863a21a557f26b5fbb82c0cf441b",
     format: "json",
   },
 });
